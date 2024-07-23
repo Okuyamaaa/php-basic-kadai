b6c9ac7e16b0377f78796f6ac8c9b685c5ed3982
Okuyamaaa <soccer.south.komae.0705@gmail.com>
2024-07-23 20:51:59 +0900

    Add files via upload

---
+++ kadai_011/foreach.php
@@ -0,0 +1,18 @@
+<!DOCTYPE>
+<html lang="ja">
+    <head>
+        <meta charset="UTF-8">
+        <title>PHP基礎編_11課題</title>
+</head>
+<body>
+    <p>
+        <?php
+        $vegetable = ["名前"=>"玉ねぎ", "値段"=>200, "産地"=>"北海道"];
+
+        foreach($vegetable as $key=>$value){
+            echo "{$key}:{$value}<br>";
+        }
+        ?>
+        </p>
+    </body>
+    </html>
